44e86239fc1dfd3fd33db4cc55cf048c248f4f73
maxicaronte2 <maxicaronte@gmail.com>
2014-08-06 20:06:20 -0300

    Agregado de VerProyecto con accordion de modulos

---
+++ App_Code/Modulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+namespace Prodeo.Entidad
+{
+    public class Modulo
+    {
+        public string nombreModulo { get; set; }
+        public DataTable tablaTareas { get; set; }
+    }
+}
