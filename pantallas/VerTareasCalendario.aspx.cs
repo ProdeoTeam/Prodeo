122a7fb9d06cc9185dc5a22c2581a6c984cb2459
maxicaronte2 <maxicaronte@gmail.com>
2016-02-12 20:59:07 -0300

    Calendario de tareas
    Calendario de tareas

---
+++ pantallas/VerTareasCalendario.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+namespace Prodeo.pantallas
+{
+    public partial class VerTareasCalendario : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [AjaxPro.AjaxMethod(AjaxPro.HttpSessionStateRequirement.ReadWrite)]
+        private ArrayList obtenerTareas()
+        {
+            ArrayList aTareas = new ArrayList();
+            ArrayList aUnaTarea = new ArrayList();
+            Negocio.ReportesLogica neg = new Negocio.ReportesLogica();
+            try
+            {
+                //Crear un array con arrays de 2 posiciones: (0)->Nombre (1)->Fecha
+
+            }
+            catch (Exception)
+            {
+                aTareas = new ArrayList();
+            }
+            return aTareas;
+        }
+    }
+}
