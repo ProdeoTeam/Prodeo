81c928301369b11b45ccca708cb7a57f995e42c5
maxicaronte2 <maxicaronte@gmail.com>
2016-04-30 11:58:24 -0300

    Va devuelta el calendario

---
+++ Datos/Reportes/ReporteCalendario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Reportes
+{
+    class ReporteCalendario
+    {
+        //Esta clase se utiliza en el javascript del FullCalendar de jQuery
+        public string title { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+
+    }
+}
