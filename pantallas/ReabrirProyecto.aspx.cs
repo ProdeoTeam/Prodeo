cd92bf8673078214bb00fc1de9f78d04ca3efc8a
gmarchese <gemarchese83@gmail.com>
2016-07-23 17:51:39 -0300

    reabrir proyecto

--- pantallas/ReabrirProyecto.aspx.cs
+++ pantallas/ReabrirProyecto.aspx.cs
@@ -4,13 +4,47 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Negocio;
+using Datos;
 
 namespace Prodeo.pantallas
 {
     public partial class ReabrirProyecto : System.Web.UI.Page
     {
+        public int proyecto;
+        public Proyectos datosPro;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["usuario"] == null)
+            {
+                Response.Redirect("~/index.aspx");
+            }
+            else
+            {
+                ProyectoLogica proy = new ProyectoLogica();
+                proyecto = Convert.ToInt32(Request.QueryString["idProyecto"]);
+                datosPro = proy.obtieneDatosProyecto(proyecto.ToString());
+                LabelElimTareas.Text = "Se va a reabrir el proyecto " + datosPro.Nombre + ", desea continuar?";
+            }
+        }
+
+        protected void cancelar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/pantallas/ListaProyectos.aspx");
+        }
+
+        protected void reabrirProyecto_Click(object sender, EventArgs e)
+        {
+            ProyectoLogica proy = new ProyectoLogica();
+            bool respuesta = proy.ReabrirProyecto(proyecto);
+            if (respuesta)
+            {
+                Response.Redirect("~/pantallas/ListaProyectos.aspx");
+            }
+            else
+            {
+                LabelElimTareas.Text = "No se pudo reabrir el proyecto " + datosPro.Nombre + "!";
+            }
 
         }
     }
